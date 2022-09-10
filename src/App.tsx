aba31bef2919f876945ffef316a9784819c03bef
zac <zacooper5g@outlook.com>
2022-09-10 20:28:01 +1000

    main app file including components
    fjkd
    !

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { SpendingCard } from './components/SpendingCard';
 
 function App() {
   return (
-    <div className="App">
-      hi
+    <div className="App p-5 m-5">
+      <SpendingCard />
     </div>
   );
 }
